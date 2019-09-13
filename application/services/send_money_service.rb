830881d5f740ca43905d3ff7be11c63c52bd69c9
Mario Viapiano <marioeviapiano@gmail.com>
2019-09-13 21:58:29 +0200

    Improve SO & Entity

--- application/services/send_money_service.rb
+++ application/services/send_money_service.rb
@@ -2,11 +2,20 @@
 
 # Service Object.
 class SendMoneyService
-  attr_reader :load_account_port, :account_lock, :update_account_state_port
+  AccountNotFound = Class.new(StandardError)
 
-  def send_money(_command)
+  def send_money(command)
     # TODO: validate business rules.
+    check_existence_of(command.source_account_id)
+    check_existence_of(command.target_account_id)
+
     # TODO: manipulate model state.
     # TODO: return output.
+  end
+
+  private
+
+  def check_existence_of
+    true || raise(AccountNotFound)
   end
 end
--- domain/account.rb
+++ domain/account.rb
@@ -2,4 +2,29 @@
 
 # Entity to represent a Account.
 class Account
+  attr_reader :id, :balance
+
+  def initialize(id:, balance:)
+    @id = id
+    @balance = balance
+  end
+
+  def withdraw(amount:, target_account:)
+    return false unless may_withdraw(amount: amount)
+
+    @balance -= amount
+    target_account.deposit(amount)
+    self
+  end
+
+  def deposit(amount:)
+    @balance += amount
+    self
+  end
+
+  private
+
+  def may_withdraw(amount:)
+    (@balance - amount) > 0
+  end
 end
