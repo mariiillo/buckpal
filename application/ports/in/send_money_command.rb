9a5c16f44dd94bfcfec2985dc9073a7fd3e13faf
Mario Viapiano <marioeviapiano@gmail.com>
2019-09-13 21:33:57 +0200

    Add dry-validation & application port

---
+++ application/ports/in/send_money_command.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require 'dry-validation'
+
+# Command class.
+# This class should take care of the validation of the data that will be sent to
+# the Service Object.
+class SendMoneyCommand < Dry::Validation::Contract
+  params do
+    required(:source_account_id).value(:integer)
+    required(:source_target_id).value(:integer)
+    required(:amount).value(:integer)
+  end
+end
